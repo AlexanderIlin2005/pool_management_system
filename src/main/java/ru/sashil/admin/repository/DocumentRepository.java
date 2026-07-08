85aece852a550cd1b7a8d5192d9b9376ef3cf48b
AlexanderIlin2005 <sashailyin2005@gmail.com>
2026-07-08 19:43:06 +0300

    fixes

--- src/main/java/ru/sashil/admin/repository/DocumentRepository.java
+++ src/main/java/ru/sashil/admin/repository/DocumentRepository.java
@@ -1,6 +1,9 @@
 package ru.sashil.admin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ru.sashil.admin.model.DocumentVersion;
 import java.util.List;
 import java.util.Optional;
@@ -8,4 +11,10 @@ import java.util.Optional;
 public interface DocumentRepository extends JpaRepository<DocumentVersion, Long> {
     Optional<DocumentVersion> findByDocTypeAndIsActiveTrue(String docType);
     List<DocumentVersion> findByDocTypeOrderByUploadedAtDesc(String docType);
+
+    // Метод для принудительного сброса всех активных документов определенного типа
+    @Modifying
+    @Transactional
+    @Query("UPDATE DocumentVersion d SET d.isActive = false WHERE d.docType = :docType")
+    void deactivateAllByDocType(String docType);
 }
